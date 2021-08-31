93d7703b0827e8002775f03a2ec7b8b58074b80d
Ben Yang <benyang2020@gmail.com>
2021-08-31 14:00:44 -0400

    commit tree ec14cac2857bdcbcae5e9b2c259e681d6b46348d

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+const headquarters = 42;
+
+function distanceFromHqInBlocks(distance1){
+    if (distance1 > 42) {
+        return (distance1 - headquarters);
+     } else if (distance1 < 42) {
+         return (headquarters - distance1);
+     }
+}
+
+const feet = 264
+function distanceFromHqInFeet(distance2) {
+if (distance2 > 42) {
+    return ((distance2 - headquarters) * feet);
+ } else if (distance2 < 42) {
+     return ((headquarters - distance2)) * feet;
+ }
+}
+
+function distanceTravelledInFeet(headquarters, distance2) {
+    if (distance2 > headquarters){
+        return (distance2 - headquarters)*feet;
+    }else{
+        return (headquarters - distance2)*feet;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+   const total = distanceTravelledInFeet(start, destination)
+   
+   if (total < 400) {
+       return 0
+   } else if ((total >= 400) && (total <= 2000)) {
+       return total * 0.02;
+ 
+   } else if (total > 2500) {
+       return 'cannot travel that far'
+    } else if (total > 2000) {
+        return 25 
+
+    }
+    }
